0d4fc4fd80d09991325db7fa1bb1d1308a2d0a30
albarmo <moerhamsa@gmail.com>
2021-04-01 17:52:22 +0700

    fixed get image from dir public

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
+
 const environment = "development";
 const express = require("express");
 const app = express();
 const port = process.env.port || 3001;
 const cors = require("cors");
 const routes = require("./routes");
+const path = require("path");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +18,12 @@ app.use(routes);
 
 app.get("/test", (req, res) => {
   res.status(200).json({ message: "Okay" });
+});
+
+// Define the static file path
+app.use(express.static(__dirname + "/public/uploads"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
