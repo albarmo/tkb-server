a4e7b4b7052a239aafd5e9ccafd84771794d3a89
albarmo <moerhamsa@gmail.com>
2021-04-01 12:57:12 +0700

    dep

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const { Pool, Client } = require("pg");
 const connectionString =
-  "postgresql://vnonnkjpdbdqlr:f2fc8bb124a0b04b9219d072a476675bedd30527e05f294b5fc0635ebf72a597@ec2-52-45-73-150.compute-1.amazonaws.com:5432/d7npe52ng392v7?ssl=true";
+  "postgresql://vnonnkjpdbdqlr:f2fc8bb124a0b04b9219d072a476675bedd30527e05f294b5fc0635ebf72a597@ec2-52-45-73-150.compute-1.amazonaws.com:5432/d7npe52ng392v7";
 
 const pool = new Pool({
   connectionString: connectionString,
+  ssl: true,
 });
 
 module.exports = pool;
