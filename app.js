99dd019410df9287cebe2b2b384851528d2e5c93
albarmo <moerhamsa@gmail.com>
2021-04-01 12:08:33 +0700

    deploy heroku change production config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.port || 3001;
 const cors = require("cors");
 const routes = require("./routes");
 
